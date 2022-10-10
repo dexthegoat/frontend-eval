69e9a4c1cb3a76640794d78ce16535845ab796f2
Jianshuo Yang <jsy.bornconfused@gmail.com>
2022-10-10 17:40:06 -0400

    did skeleton

--- selectable-grid/src/App.js
+++ selectable-grid/src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 
-function getArr() {
-  const arr = [];
-  for (let i = 0; i < 100; i++) {
-    arr.push(i + 1);
-  }
-  return arr;
-}
-
 function App() {
   return (
     <div className="container">
-      {getArr().map((_, idx) => (
-        <div key={`grid${idx + 1}`} className="grid"></div>
-      ))}
+      {Array(100)
+        .fill(0)
+        .map((_, idx) => (
+          <div key={`grid${idx + 1}`} className="grid"></div>
+        ))}
     </div>
   );
 }

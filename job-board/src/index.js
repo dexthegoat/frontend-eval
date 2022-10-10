6befbdaac3757976eb76a6ff02dabf1f2fa74c16
Jianshuo Yang <jsy.bornconfused@gmail.com>
2022-10-10 06:32:44 -0400

    .

--- job-board/src/index.js
+++ job-board/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// In the strict mode of React 18 an effect with useEffect seems to be called twice.
 root.render(
   <React.StrictMode>
     <App />
